c7ed1c06084033f80dee6da2e350f9c8962086e9
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-29 22:40:35 +0530

    Marshalling JSON

--- product-api/main.go
+++ product-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,6 +16,16 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
+type helloWorldResponse struct {
+	Message string `json:"message"`
+}
+
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!\n")
+	response := helloWorldResponse{Message: "Hello world!"}
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		panic("Oops")
+	}
+	fmt.Fprintf(w, string(data))
 }
